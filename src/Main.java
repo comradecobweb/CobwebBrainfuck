90f6375fd662a0dc0c1b6d989216ada6d3317860
comradecobweb <comradecobweb@gmail.com>
2025-02-15 14:13:53 +0100

    Refactorized Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
+import exceptions.ArgumentException;
 import exceptions.BracketException;
 import exceptions.InterpretationException;
 
-public class Main {
+public class Main extends Application{
     public static void main(String[] args) {
+        try {
+            new Main(args).run();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    Main(String[] args) {
+        super(args);
+    }
+
+    @Override
+    public void run() throws ArgumentException {
         String path = "";
         int length = 0;
-
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
+        for (; position < arguments.length; position++)
+             switch (arguments[position]) {
                 case "memory", "-memory", "--memory", "mem", "-mem", "--mem":
-                    length = getNextInt(args, i);
-                    if (i != args.length - 1) i++;
+                    length = getNextInt();
                     break;
                 case "path", "-path", "--path":
-                    path = getNextArg(args, i);
-                    if (i != args.length - 1) i++;
+                    path = getNextArgument();
                     break;
                 default:
-                    System.err.println("Error: Unknown argument: " + args[i] + "\n");
                     System.exit(1);
             }
-        }
         run(path, length);
     }
 
-    public static void run(String path, int length) {
+    private static void run(String path, int length) {
         if (path.isEmpty()) {
             System.err.println("Error: Input file is not specified!");
             System.exit(1);
@@ -32,36 +41,9 @@ public class Main {
         try {
             Interpreter i = new Interpreter(path, length);
             i.execute();
-        }catch (BracketException b) {
-            System.err.println("Error: " + b.getMessage() + "\n\tat: ");
-            System.exit(1);
-        }catch (InterpretationException e){
-            System.out.println("Error: " + e.getMessage() + "\n\tat: ");
-        }
-    }
-
-    private static String getNextArg(String[] args, int i) {
-        if (i == args.length - 1) {
-            System.err.println("Error: Value for argument " + args[i] + " not specified!");
+        }catch (BracketException | InterpretationException b) {
+            System.err.println("Error: " + b.getMessage());
             System.exit(1);
         }
-
-        return args[i + 1];
-    }
-
-    private static int getNextInt(String[] args, int i) {
-        int value = 0;
-        try {
-            value = Integer.parseInt(getNextArg(args, i));
-        } catch (NumberFormatException e) {
-            System.err.println("Error: Value for argument " + args[i] + " is not a number!");
-            System.exit(1);
-        }
-        if (value < 2) {
-            System.err.println("Error: Value for argument " + args[i] + " is too low!");
-            System.exit(1);
-        }
-
-        return -1;
     }
 }
