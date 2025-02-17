e560e47098556cbb11365451f971e899ee85b896
comradecobweb <comradecobweb@gmail.com>
2025-02-17 13:51:29 +0100

    Added Instructions.kt

---
+++ src/Instructions.kt
@@ -0,0 +1,10 @@
+class Instructions(
+    val plus: String,
+    val minus: String,
+    val leftArrow: String,
+    val rightArrow: String,
+    val comma: String,
+    val dot: String,
+    val leftBracket: String,
+    val rightBracket: String
+)
